c9feb27f3421c49181ad87849c74f372e2b9968e
MORGANE DEBARGE <morganedebarge@msn.com>
2023-11-15 14:03:36 +0100

    image en import

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,8 @@
 import { NavLink } from "react-router-dom";
-
+import logo from "../assets/logo.png";
+import chaudron from "../assets/chaudron.png";
+import potion from "../assets/potion.svg";
+import trophy from "../assets/trophy.svg";
 import PlayingMusic from "./Music";
 
 function NavBar() {
@@ -7,22 +10,18 @@ function NavBar() {
     <nav>
       <NavLink to="/JS-RemoteFR-SACOD-P2-Vogue-Merry/">
         <div className="head">
-          <img id="logo" src="/JS-RemoteFR-SACOD-P2-Vogue-Merry/assets/logo.png" alt="logo" />
+          <img id="logo" src={logo} alt="logo" />
           <div className="title">
             <h1>Elixir Maker</h1>
             <p>TEST YOUR KNOWLEDGE</p>
           </div>
         </div>
       </NavLink>
-      <ul className="/JS-RemoteFR-SACOD-P2-Vogue-Merry/leaderboard">
+      <ul className="leaderboard">
         <li>
-          <NavLink to="difficulty">
+          <NavLink to="/JS-RemoteFR-SACOD-P2-Vogue-Merry/difficulty">
             <button id="playButton" type="button">
-              <img
-                id="potion"
-                src="/JS-RemoteFR-SACOD-P2-Vogue-Merry/assets/chaudron.png"
-                alt="chaudron"
-              />
+              <img id="potion" src={chaudron} alt="chaudron" />
               <h1>Play</h1>
             </button>
           </NavLink>
@@ -30,7 +29,7 @@ function NavBar() {
         <li>
           <NavLink to="/JS-RemoteFR-SACOD-P2-Vogue-Merry/elixir">
             <button id="potionButton" type="button">
-              <img id="potion" src="/JS-RemoteFR-SACOD-P2-Vogue-Merry/assets/potion.svg" alt="potion" />
+              <img id="potion" src={potion} alt="potion" />
               <h1>Elixirs</h1>
             </button>
           </NavLink>
@@ -38,7 +37,7 @@ function NavBar() {
         <li>
           <NavLink to="/JS-RemoteFR-SACOD-P2-Vogue-Merry/leaderboard">
             <button id="trophyButton" type="button">
-              <img id="trophy" src="/JS-RemoteFR-SACOD-P2-Vogue-Merry/assets/trophy.svg" alt="trophy" />
+              <img id="trophy" src={trophy} alt="trophy" />
               <h1>Leaderboard</h1>
             </button>
           </NavLink>
